31c6cb724925539351c6c178949b250f57c5ac73
harshanmad <harshanmadhurangaw@gmail.com>
2019-12-19 12:38:53 +0530

    first commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Image;
+
+class ImageController extends Controller
+{
+    //
+    public function resizeImage(){
+        return view('resizeImage');
+    }
+    
+    public function resizeImagePost(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+  
+        $image = $request->file('image');
+        $input['imagename'] = time().'.'.$image->extension();
+     
+        $destinationPath = public_path('/thumbnail');
+        $img = Image::make($image->path());
+        $img->resize(100, 100, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$input['imagename']);
+   
+        $destinationPath = public_path('/images');
+        $image->move($destinationPath, $input['imagename']);
+   
+        return back()
+            ->with('success','Image Upload successful')
+            ->with('imageName',$input['imagename']);
+    }
+}
